9da9bae123dc94a6858b49a4ac2b246da39b1c1b
GRUZZLER <christiangriessel@gmail.com>
2024-09-16 20:20:50 -0700

    Short story print statement assignment
    thrilling! chilling! amazing! adventures!

---
+++ cis129_lab02_shortStory.py.py
@@ -0,0 +1,8 @@
+# Author: Christian Griessel
+# Thrilling Story Program for CIS 129
+# This utilizes not one, not two, not three, but FOUR print statements to tell an engaging story!
+
+print("This is a thrilling beginning\n\n")
+print("What's this? An engaging middle? The prodigal son returns home? The heroes journey?\n\n")
+print("A satisfying conclusion! Wow!\n\n")
+print("THIS STORY HAS ENDED VACATE YOUR SEAT NOW")
