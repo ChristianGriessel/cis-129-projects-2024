ce79e87fa2be18bebbbfc34b0a49dd71a24fa177
ChristianGriessel <christiangriessel@gmail.com>
2024-10-30 13:16:03 -0700

    slight logic alteration

--- cis129_extraCredit_piglatinMaker.py
+++ cis129_extraCredit_piglatinMaker.py
@@ -41,15 +41,12 @@ def pig_it(text):
     return ' '.join(pig_list)
 
 def capitalize_sentences(text):
-    #full disclosure, I couldn't for the life of me get this to work so I got a friend who is a professional programer to help me with it lol
-    #If this seems like the most advanced thing in the program that's because I had help writing it
     import re
 
-    #this tells Python to scan my sentence and split it into a list wherever it sees punctiation followed by a space
+    # this tells Python to scan my sentence and split it into a list wherever it sees punctiation followed by a space
     sentences = re.split('([.!?] *)', text)
-    #This checks if we are at an even or odd indecies in the sentence. We know all letters will be even and all punctuation will be odd
-    #If it is even we capitalize the sentence. If it isn't we just add it straight back into the list because we know it's punctuation
-    capitalized_sentences = ''.join([sentences[i].capitalize() if i % 2 == 0 else sentences[i] for i in range(len(sentences))])
+    # This capitalizes everything then the list is put back together
+    capitalized_sentences = ''.join([sentences[i].capitalize() for i in range(len(sentences))])
     return capitalized_sentences
 
 
